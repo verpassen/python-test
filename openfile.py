d09c0b930684fee0b64b0f95b414a139f1848b96
verpassen <zzzing760925@gmail.com>
2017-02-20 22:57:16 +0800

    Add files via upload

--- openfile.py
+++ openfile.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 from string import rstrip
 import numpy as np
+import scipy.fftpack
 import matplotlib.pyplot as plt
 #---2017.02.01 ---#
 #--- open txt file ---#
@@ -19,16 +20,30 @@ with open(filepath) as fid:
 		data = row.split('\t')
 		#print row
 		try:
-			time.append(data[0])
-			accel_x.append(data[1])
-			accel_y.append(data[2])
-			accel_z.append(data[3])
+			time.append(float(data[0]))
+			accel_x.append(float(data[1]))
+			accel_y.append(float(data[2]))
+			accel_z.append(float(data[3]))
 		except IndexError:
 			print 'You have an empty row'
-
+		except ValueError:
+			print 'Could not convert string to float'
+			print 'pass this item!'
+			
 #print type(time)
 #print time[0:3]
 
+#---- processing ----#
+N = len(time)
+dt = time[2]-time[1]
+f = 1./dt
+df = f/N
+freq = np.linspace(0,f/2,N/2)
+FFT_accel_x = 2.0/N*abs(scipy.fftpack.fft(accel_x)[0:N/2])
+FFT_accel_y = 2.0/N*abs(scipy.fftpack.fft(accel_y)[0:N/2])
+FFT_accel_z = 2.0/N*abs(scipy.fftpack.fft(accel_z)[0:N/2])
+
+#---plot diagram ---#
 plt.figure(1)
 plt.subplot(311)
 plt.plot(time[1:],accel_x[1:])
@@ -39,4 +54,12 @@ plt.grid()
 plt.subplot(313)
 plt.plot(time[1:],accel_z[1:])
 plt.grid()
+plt.figure(2)
+plt.subplot(311)
+plt.plot(freq,FFT_accel_x)
+plt.subplot(312)
+plt.plot(freq,FFT_accel_y)
+plt.subplot(313)
+plt.plot(freq,FFT_accel_z)
+
 plt.show()
