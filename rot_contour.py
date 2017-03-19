e1919b0ca9e61eda9a0f5dd08d164e12790e913d
verpassen <zzzing760925@gmail.com>
2017-03-19 09:40:09 +0800

    Update rot_contour.py

--- rot_contour.py
+++ rot_contour.py
@@ -6,24 +6,25 @@ def fun(n):
 	k = np.linspace(0,1,n)
 	x = 2*k+3
 	y = 2*k**2+3
-	return np.asarray([x,y,1,1])
+	return np.asarray([x,y,np.zeros(n),np.ones(n)])
 
 def rot_X_axis(rot_ang,P):
-	rad_ang = rad2deg(rot_ang)
-	M = np.asarray([[cos(rot_ang),-sin(rot_ang),0,0],[sin(rot_ang),cos(rot_ang),0,0],[0,0,1,0],[0,0,0,1]])
-	P2 = np.dot(M,P.T)
-	return P2
+	rot_ang = rad2deg(rot_ang)
+	P2_X = np.outer(np.ones(len(rot_ang)),P[0,:])
+	P2_Y = np.outer(cos(rot_ang),P[1,:]) + np.outer(sin(rot_ang),P[2,:])
+	P2_Z = np.outer(-sin(rot_ang),P[1,:]) + np.outer(cos(rot_ang),P[2,:])
+	return P2_X,P2_Y,P2_Z
 def rad2deg(rad):
 	return rad*180/pi
 
-N = 2**8
+N = 2**6
 phi = np.linspace(0,2*pi,N)
 contour_A = fun(N)
-
-
+surface_AX,surface_AY,surface_AZ= rot_X_axis(phi,contour_A)
 fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(contour_A[0],contour_A[1])
+ax = fig.add_subplot(111,projection = '3d')
+plt.plot(contour_A[0],contour_A[1],'r')
+ax.plot_surface(surface_AX,surface_AY,surface_AZ)
 ax.set_xlabel('X axis [mm]')
 ax.set_ylabel('Y axis [mm]')
 ax.set_xlim([0,10])
