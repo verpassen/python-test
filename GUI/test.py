94d25e5294f80299a76fdd8e3bfbf3e3af585d73
Brent <zzzing760925@gmail.com>
2018-08-11 20:18:10 +0800

    Add files via upload

--- GUI/test.py
+++ GUI/test.py
@@ -39,22 +39,10 @@ def vp_start_gui():
     root.mainloop()
 
 w = None
-'''
-def create_Polygon_Tool_Path(root, *args, **kwargs):
-    #Starting point when module is imported by another program.
-    global w, w_win, rt
-    rt = root
-    w = Toplevel (root)
-    test_support.set_Tk_var()
-    top = Polygon_Tool_Path (w)
-    test_support.init(w, top, *args, **kwargs)
-    return (w, top)
-'''
 def destroy_Polygon_Tool_Path():
     global w
     w.destroy()
     w = None
-
 
 class Polygon_Tool_Path:
     def __init__(self, top=None):
@@ -77,7 +65,6 @@ class Polygon_Tool_Path:
         self.Frame1.configure(borderwidth="2")
         self.Frame1.configure(relief=GROOVE)
         self.Frame1.configure(width=295)
-        
         
         self.Lt = Entry(top)
         self.Lt.place(relx=0.58, rely=0.11, relheight=0.04, relwidth=0.24)
@@ -114,7 +101,7 @@ class Polygon_Tool_Path:
         self.Button2.configure(activebackground="#d9d9d9")
         self.Button2.configure(command=test_support.destroy_window)
         self.Button2.configure(text='''Cancel''')
-        self.Button2.bind('<B1-Key>',lambda p:test_support.Button2Click(p))
+        self.Button2.bind('<B1-Key>', quit)
 
         self.n = Entry(top)
         self.n.place(relx=0.58, rely=0.38, relheight=0.04, relwidth=0.24)
@@ -152,41 +139,17 @@ class Polygon_Tool_Path:
         self.Rf.configure(selectbackground="#c4c4c4")
         self.Rf.configure(textvariable=test_support.Rf)
         
-        self.Checkbutton1 = Checkbutton(top)
-        self.Checkbutton1.place(relx=0.03, rely=0.67, relheight=0.05
-                , relwidth=0.14)
-        self.Checkbutton1.configure(activebackground="#d9d9d9")
-        self.Checkbutton1.configure(justify=LEFT)
-        self.Checkbutton1.configure(text='''Tool Path''')
-        self.Checkbutton1.configure(textvariable=test_support.on_ToolPath)
-        self.Checkbutton1.configure(variable=test_support.che46)
-
-        self.Checkbutton2 = Checkbutton(top)
-        self.Checkbutton2.place(relx=0.03, rely=0.73, relheight=0.05
-                , relwidth=0.09)
-        self.Checkbutton2.configure(activebackground="#d9d9d9")
-        self.Checkbutton2.configure(justify=LEFT)
-        self.Checkbutton2.configure(text='''Tool''')
-        self.Checkbutton2.configure(textvariable=test_support.on_Tool)
-        self.Checkbutton2.configure(variable=test_support.che47)
-
-        self.Checkbutton3 = Checkbutton(top)
-        self.Checkbutton3.place(relx=0.03, rely=0.8, relheight=0.05
-                , relwidth=0.12)
-        self.Checkbutton3.configure(activebackground="#d9d9d9")
-        self.Checkbutton3.configure(justify=LEFT)
-        self.Checkbutton3.configure(text='''Spindle''')
-        self.Checkbutton3.configure(textvariable=test_support.on_Spindle)
-        self.Checkbutton3.configure(variable=test_support.che48)
-        self.menubar = Menu(top,font="TkMenuFont",bg=_bgcolor,fg=_fgcolor)
-        top.configure(menu = self.menubar)
-
         self.Button3 = Button(top)
         self.Button3.place(relx=0.5, rely=0.82, height=30, width=57)
         self.Button3.configure(activebackground="#d9d9d9")
         self.Button3.configure(command=self.DraftClear)
         self.Button3.configure(text='''Clear''')
-        
+
+        self.f=Figure(figsize=(10,10),dpi=80)
+        self.canvas=FigureCanvasTkAgg(self.f,self.Frame1)
+        self.canvas.get_tk_widget().pack(expand=1)
+        toolbar = NavigationToolbar2TkAgg(self.canvas,top)
+        toolbar.update()
         # Entry default value        
         test_support.Lt.set(78)
         test_support.Rf.set(20)
@@ -195,11 +158,11 @@ class Polygon_Tool_Path:
         test_support.Ang_delta.set(1)
         
     def show(self):
+        self.get_Val()
         self.calculation()
-        #self.canvas = FigureCanvasTkAgg(self.f,self.Frame1)
-        #self.canvas.get_tk_widget().pack()
+        self.canvas.draw()
         self.Frame1.update()
-        print('buton pressed')
+
     def get_Val(self):
         global n,Rf,Rc,Ang_delta,Lt
         Lt = float(self.Lt.get())
@@ -209,8 +172,7 @@ class Polygon_Tool_Path:
         Ang_delta = float(self.Ang_delta.get())
    
     def calculation(self):
-        self.get_Val()
-        print(n,Rf,Rc,Ang_delta,Lt)
+        #print(n,Rf,Rc,Ang_delta,Lt)
         S = Rc/cos(pi/n) 
         r2 = Rf/Rc*S
         Upper_phi = arccos(Rc/r2)
@@ -237,7 +199,6 @@ class Polygon_Tool_Path:
         		X.append(S/(1+tan(the[k])*tan(pi/n)))
         		Y.append(tan(the[k])*X[k])
         		
-        self.f = Figure(figsize=(5,5),dpi=80)
         a = self.f.add_subplot(111,aspect='equal')
         a.add_patch(patches.Circle((0,0),Rc,fill=False))
         for i in range(n):
@@ -248,8 +209,7 @@ class Polygon_Tool_Path:
         		P_x.append(tmp[0,0])
         		P_y.append(tmp[0,1])
         		D_p.append(sqrt(tmp[0,0]**2+tmp[0,1]**2))
-        
-         
+
         phi = np.linspace(0,2*pi,n*360)
         for i in range(len(phi)):
         	P1 = np.array([-Lt,0,1])
@@ -257,23 +217,13 @@ class Polygon_Tool_Path:
         	P2 = np.dot(M12,P1)
         	S_x.append(P2[0,0])
         	S_y.append(P2[0,1])
-        		
         a.plot(P_x,P_y)	
         a.plot(S_x,S_y)	
         
-        self.canvas = FigureCanvasTkAgg(self.f,self.Frame1)
-        self.canvas.get_tk_widget().pack()
- 
     def DraftClear(self):
-        #self.Frame1.destory()
-        pass
-        '''
-        f.clf()
-        self.canvas = FigureCanvasTkAgg(self.f,self.Frame1)
-        self.canvas.get_tk_widget().pack()
-        '''
-    def on_press(self,event):
-        'when "ok" is pressed '
+        self.f.clear()
+        self.canvas.draw()
+        
         
 if __name__ == '__main__':
     vp_start_gui()
